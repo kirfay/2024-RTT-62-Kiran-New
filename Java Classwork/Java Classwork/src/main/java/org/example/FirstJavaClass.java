2e851a98e1308c8e5b2743959d8d235e237b0f7d
Kiran Fayyaz <kiranfay0611@gmail.com>
2024-05-09 09:03:58 -0700

    GLAB 303.2.1 and GLAB 303.2.2

---
+++ Java Classwork/Java Classwork/src/main/java/org/example/FirstJavaClass.java
@@ -0,0 +1,34 @@
+package org.example;
+
+public class FirstJavaClass {
+    public static void main(String[] args) {
+        byte num;
+        num = 113;
+        System.out.println(num);
+
+        short snum;
+        snum = 150;
+        System.out.println(snum);
+
+        long lnum = -123322552626L;
+        System.out.println(lnum);
+
+        double dnum = -42937737.9d;
+        System.out.println(dnum);
+
+        float fnum = 19.98f;
+        System.out.println(fnum);
+
+        boolean b = false;
+        System.out.println(b);
+
+        char ch = 'Z';
+        System.out.println(ch);
+
+        //
+        int zero = 4 % 2;
+        int one = 5 % 2;
+        //Modulus is an important concept to know
+        //S
+    }
+}
