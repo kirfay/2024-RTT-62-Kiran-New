8247425b686e59f0148c1e10bf61a1f941dda3c0
Kiran Fayyaz <kiranfay0611@gmail.com>
2024-05-10 07:04:25 -0700

    GLAB 303.10.1

---
+++ Java Classwork/Java Classwork/src/main/java/org/example/GLAB_303_10_1/Boy.java
@@ -0,0 +1,24 @@
+package org.example.GLAB_303_10_1;
+
+public class Boy {
+    public static boolean lifeSpan() {
+    }
+
+    public boolean talk() {
+    }
+
+    public boolean walk() {
+    }
+
+    public class Boy extends Person {
+        static double ageFactor = 1.1;
+
+        public String talk() {
+            return (super.talk() + " ... but I love Java class.");
+        }
+
+        public String walk() {
+            return ("I am now walking");
+        }
+    }
+}
---
+++ Java Classwork/Java Classwork/src/main/java/org/example/slides_303_9_Inheritance/PerimeterInterface.java
@@ -0,0 +1,4 @@
+package org.example.slides_303_9_Inheritance;
+
+public class PerimeterInterface {
+}
