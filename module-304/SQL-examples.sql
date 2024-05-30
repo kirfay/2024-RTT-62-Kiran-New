edf7d7a99c16629fe588331e9016097c31a9c231
Kiran Fayyaz <kiranfay0611@gmail.com>
2024-05-30 11:33:42 -0700

    sql examples

--- module-304/SQL-examples.sql
+++ module-304/SQL-examples.sql
@@ -1,1 +1,14 @@
 select customer_name, phone, address_line1 from customers;
+select * from customers;
+select * 
+from customers
+where contact_firstname Like'%gmail.com';
+select * 
+from customers
+where contact_firstname Like'%tt%';
+select * 
+from customers
+where customer_name Like'%Inc';
+select *
+from customers
+where phone like '%212%';
