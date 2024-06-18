274c289599692fff3cb0606e4c13352654a54f4a
Kiran Fayyaz <kiranfay0611@gmail.com>
2024-06-18 09:18:10 -0700

    module 307 practice and module 305

--- Module--305/M305-JDBC/src/main/java/org/example/JDBCDemo.java
+++ Module--305/M305-JDBC/src/main/java/org/example/JDBCDemo.java
@@ -1,5 +1,6 @@
 package org.example;
 import java.sql.*;
+import java.util.Scanner;
 
 public class JDBCDemo {
     public static void main(String[] args) throws ClassNotFoundException {
@@ -7,13 +8,28 @@ public class JDBCDemo {
         String user = "root";
         String password = "Ammomin24";
         System.out.println("-------- MySQL JDBC Connection Demo ------------");
+
+
+
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection = DriverManager.getConnection(dburl, user, password);
-            String SelectSQL = "Select * FROM employees";
-            Statement stmt = connection.createStatement();
-            ResultSet result =  stmt.executeQuery(SelectSQL);
-            while(result.next())
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter the first name of the employee you want to search for: ");
+            String input = scanner.nextLine();
+            String lastnameInput = "Doe";
+            String SelectSQL = "Select * FROM employees where firstname = ? or lastname = ? ";
+
+            //Statement stmt = connection.createStatement();
+            PreparedStatement stmt = connection.prepareStatement(SelectSQL);
+            stmt.setString(1,input);
+            stmt.setString(2,lastnameInput);
+
+
+            ResultSet result =  stmt.executeQuery();
+
+            while(result.next())  //is there a next record//
             {
                 String employeeID  = result.getString("id");
                 String fname = result.getString("firstname");
@@ -21,10 +37,11 @@ public class JDBCDemo {
                 String email = result.getString("email");
                 String job_title = result.getString("job_title");
                 String format = "%-10s | %-15s | %-15s | %-30s | %-20s%n";
-
+                System.out.println(employeeID + " | " + fname + " | " + lname + " | " + email + " | " + job_title);
 // Use printf to format the output
 
-                System.out.printf(format,id,fname,lname,email,job_title);
+
+
             }
             connection.close();
         }
---
+++ Module--305/M305-JDBC/src/main/java/org/example/database/entity/Employee.java
@@ -0,0 +1,14 @@
+package org.example.database.entity;
+
+public class Employee {
+    private int id;
+    private Integer officeId;
+    private String lastname;
+    private String firstname;
+    private String extension;
+    private String email;
+    private String reportsTo;
+    private String jobTitle;
+
+
+}
