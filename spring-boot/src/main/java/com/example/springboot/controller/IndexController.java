baf5d7db37155783cc07c127b54322bbe584856a
Kiran Fayyaz <kiranfay0611@gmail.com>
2024-07-08 11:23:58 -0700

    Created the spring boot project

---
+++ spring-boot/src/main/java/com/example/springboot/controller/IndexController.java
@@ -0,0 +1,23 @@
+package com.example.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public ModelAndView index() {
+        // this function is for the home page of the website which is the expressed as just a plain slash "/"
+        ModelAndView response = new ModelAndView("index");
+        return response;
+    }
+
+    @GetMapping("/page-url")
+    public ModelAndView anotherPage() {
+        ModelAndView response = new ModelAndView("another-page");
+
+        return response;
+    }
+
+}
